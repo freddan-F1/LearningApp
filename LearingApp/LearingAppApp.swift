ab7d22195cc1d5858a58b4927e8c032d29368f85
Fredrik Nilsson <fredrik.karlsbyn@gmail.com>
2022-10-30 10:47:49 +0100

    Initial Commit

---
+++ LearingApp/LearingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearingAppApp.swift
+//  LearingApp
+//
+//  Created by Fredrik Nilsson on 2022-10-30.
+//
+
+import SwiftUI
+
+@main
+struct LearingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
