31c1be1661db1327ee5efb8bd337e649553eda53
Fredrik Nilsson <fredrik.karlsbyn@gmail.com>
2022-10-30 15:05:46 +0100

    add assets add data and style files add modules

---
+++ LearingApp/Models/Models.swift
@@ -0,0 +1,51 @@
+//
+//  Models.swift
+//  LearingApp
+//
+//  Created by Fredrik Nilsson on 2022-10-30.
+//
+
+import Foundation
+
+struct Module: Decodable, Identifiable {
+ 
+    var id:Int
+    var category:String
+    var content:Content
+    var test:Test
+}
+
+struct Content:Decodable, Identifiable {
+    
+    var id:Int
+    var image:String
+    var time:String
+    var description:String
+    var lessons:[Lesson]
+}
+
+struct Lesson:Decodable, Identifiable {
+    
+    var id:Int
+    var title:String
+    var video:String
+    var duration:String
+    var explanation:String
+}
+
+struct Test:Decodable, Identifiable {
+    
+    var id:Int
+    var image:String
+    var time:String
+    var descripition:String
+    var questions:[Question]
+}
+
+struct Question:Decodable, Identifiable {
+    
+    var id:Int
+    var content:String
+    var correctIndex:Int
+    var answers:[String]
+}
